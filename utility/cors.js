3518b4e0a959b0b3767880d04216f97bb14fd6ee
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2015-10-10 00:18:26 +0200

    cos

--- utility/cors.js
+++ utility/cors.js
@@ -1,5 +1,5 @@
 module.exports = function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", process.env.CORS);
+    res.header("Access-Control-Allow-Origin", process.env.CORS_URL);
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header("Access-Control-Allow-Methods", "GET");
     next();
